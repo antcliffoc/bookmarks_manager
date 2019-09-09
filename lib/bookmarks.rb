9a15483b56c2d51e77ac32d2ab0f917e84d2eced
karlo <deguzmankarlo@gmail.com>
2019-09-09 15:59:24 +0100

    Connected to database

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 class Bookmarks
   attr_reader :bookmarks
 
@@ -10,6 +12,8 @@ class Bookmarks
   end
 
   def all
-    @bookmarks
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 end
